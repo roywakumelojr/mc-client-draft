bfd95a6a22071b4b222b89cfafb7c3f3885b7c2c
Roy Wakumelo <roywakumelojr@gmail.com>
2020-01-19 20:30:52 -0500

    Code clean up

--- src/components/Authorization/index.jsx
+++ src/components/Authorization/index.jsx
@@ -6,15 +6,9 @@ import Layout from '../Layout';
 import Project from '../Project/Project';
 import ProjectListView from '../ProjectListView';
 
-// OKTA authentication widget, invokes implicit callback to login
-// if the user attempts to access a protected view.
-// isAuthenticated peers into storage for a valid token upon PKCE
-// handshake, so clear localStorage is called on logout.
-
 export default withAuth(({ auth }) => {
   const [authState, setAuthState] = useState(null);
   const { isAuthenticated, logout, getAccessToken } = auth;
-  // Memoized callback for subsequent re-renders of Dashboard children
   const checkAuthentication = useCallback(async () => {
     const authenticated = await isAuthenticated();
     if (authenticated !== authState) {
@@ -22,7 +16,6 @@ export default withAuth(({ auth }) => {
     }
   }, [authState, isAuthenticated]);
 
-  // useCallback on re-render if dependencies are valid
   useEffect(() => {
     checkAuthentication();
   });
